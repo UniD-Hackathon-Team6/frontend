cd3fca86f65dbfa895e01e79f6ae60636aa04bd5
윤예슬 <yoon@yun-yeseul-ui-MacBookPro.local>
2022-11-05 17:25:54 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return <SafeAreaView><Text>uni-dthon team6</Text></SafeAreaView>
+}
